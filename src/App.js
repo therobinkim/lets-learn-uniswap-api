a659fd960dd50f390c21ab08ef5fad8e2f2bf89d
Robin Kim <robincodes@pm.me>
2022-04-30 15:54:08 -0500

    Fetch tokens data from API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+const GRAPH_API = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2';
+
+const GET_TOKEN_DAY_DATAS = {};
+
+const GET_TOKENS = {"query":"{\n  tokens(first: 10, orderBy: tradeVolumeUSD, orderDirection: desc) {\n    id\n    symbol\n    name\n    tradeVolumeUSD\n    untrackedVolumeUSD\n  }\n}","variables":null,"extensions":{"headers":null}};
+
+const states = {
+  LOADING: 'LOADING',
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+}
 
 function App() {
+  const [status, setStatus] = useState(states.LOADING);
+  const [tokens, setTokens] = useState([]);
+
+  useEffect(function fetchData() {
+    fetch(GRAPH_API, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(GET_TOKENS),
+    })
+    .then(response => response.json())
+    .then(response => {
+      setTokens(response);
+    })
+    .catch((error)=>{
+      // log error to bug monitoring tool
+      console.error(error); // for dev only
+      setStatus(states.ERROR);
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Top 10 tokens at Uniswap.
+        </h1>
+        <p>{JSON.stringify(tokens)}</p>
+        {/* <TokensTable
+          tokens={tokens}
+        /> */}
       </header>
     </div>
   );
